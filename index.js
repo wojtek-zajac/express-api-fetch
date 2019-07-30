336412294b69a4c3252bccf9d2ea7bae22b942f9
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-07-30 12:59:52 +0200

    Use request-promise-native

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 8051;
-const fetch = require('node-fetch');
+const rp = require('request-promise-native');
+const options = {
+    uri: 'https://api.smartrecruiters.com/candidates',
+    headers: {
+        'X-SmartToken': 'db4b3671a5314890b7c039fdd9acf7cf'
+    },
+    json: true
+}
 
-app.get('/', function (req, res) {
-    var url = 'https://api.smartrecruiters.com/v1/companies/PIQC/postings';
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            res.send({data});
+app.get('/', (req, res) => {
+    rp(options)
+        .then(parsedBody => {
+            res.send(parsedBody);
         })
         .catch(err => {
-            res.send(err)
-        })
+            res.send(err);
+        });
 });
+
 
 app.listen(port, () => console.log(`The express-api-fetch app listening on port ${port}!`));
