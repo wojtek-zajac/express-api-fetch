ede03cf9952517f50e312a576bfc700ab535fc13
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-08-05 22:49:57 +0200

    Add root route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8051;
 
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
 // ASSESSMENTS
 
 app.get('/assessments', (req, res) => {
