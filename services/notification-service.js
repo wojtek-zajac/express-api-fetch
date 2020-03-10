c5e3d319454f0588dd8c075e25a266048f3196bf
Wojciech Zając <wojciechzajaaac@gmail.com>
2020-03-11 00:04:58 +0100

    Chain methods

--- services/notification-service.js
+++ services/notification-service.js
@@ -3,15 +3,19 @@ const mailingService = require('./mailing-service')
 
 module.exports = {
     processAssessmentOrder: (req, res) => {
-        let candidate = 'n'
+        let candidate
         assessmentService.getAssessmentById(req.body.assessmentOrderId)
             .then(response => {
-                candidate = response.candidate.email
+                return candidate = response.candidate.email
             })
-        mailingService.sendEmail(candidate)
-        assessmentService.acceptAssessment(req.body.assessmentOrderId, candidate)
-            .then(response => {
-                res.send(response)
+            .then((candidate) => {
+                mailingService.sendEmail(candidate)
+            })
+            .finally(() => {
+                assessmentService.acceptAssessment(req.body.assessmentOrderId, candidate)
+                    .then(response => {
+                        res.send(response)
+                    })
             })
     }
 }
