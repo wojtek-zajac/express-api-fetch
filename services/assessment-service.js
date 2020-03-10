c2912f6c7ea147a911eade1ed53bc0e4874baff9
Wojciech Zając <wojciechzajaaac@gmail.com>
2020-03-10 23:51:42 +0100

    Fix typo

--- services/assessment-service.js
+++ services/assessment-service.js
@@ -23,8 +23,8 @@ module.exports = {
     })
         .then(res => res.json()),
 
-    acceptAssessment: (assesmentOrderId) => {
-        const body = {message: 'This is an accept message'}
+    acceptAssessment: (assesmentOrderId, candidate) => {
+        const body = {message: `This is an accept message ${candidate}`}
         return fetch(`https://api.smartrecruiters.com/v1/assessments/${assesmentOrderId}/accept`, {
             method: 'POST',
             body: JSON.stringify(body),
--- services/notification-service.js
+++ services/notification-service.js
@@ -1,16 +1,17 @@
 const assessmentService = require('./assessment-service')
-//const mailingService = require('./mailing-service')
+const mailingService = require('./mailing-service')
 
 module.exports = {
     processAssessmentOrder: (req, res) => {
-        //let candidate = 'n'
-        // assessmentService.getAssessmentById(req.body.assessmentOrderId)
-        //     .then(response => {
-        //         candidate = response.candidate.email
-        //     })
-        //mailingService.sendEmail(candidate)
-        assessmentService.acceptAssessment(req.body.assessmentOrderId)
+        let candidate = 'n'
+        assessmentService.getAssessmentById(req.body.assessmentOrderId)
+            .then(response => {
+                candidate = response.candidate.email
+            })
+        mailingService.sendEmail(candidate)
+        assessmentService.acceptAssessment(req.body.assessmentOrderId, candidate)
             .then(response => {
                 res.send(response)
             })
     }
+}
