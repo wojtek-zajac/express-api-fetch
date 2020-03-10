d6c2f8edc7ff55658f5287e3a7fe4958d0331551
Wojciech Zając <wojciechzajaaac@gmail.com>
2020-03-10 23:42:16 +0100

    Mock candidate name

--- services/notification-service.js
+++ services/notification-service.js
@@ -3,13 +3,13 @@ const assessmentService = require('./assessment-service')
 
 module.exports = {
     processAssessmentOrder: (req, res) => {
-        let candidate = 'n'
+        //let candidate = 'n'
         // assessmentService.getAssessmentById(req.body.assessmentOrderId)
         //     .then(response => {
         //         candidate = response.candidate.email
         //     })
         //mailingService.sendEmail(candidate)
-        assessmentService.acceptAssessment(req.body.assessmentOrderId, candidate)
+        assessmentService.acceptAssessment(req.body.assessmentOrderId, 'TEST')
             .then(response => {
                 res.send(response)
             })
