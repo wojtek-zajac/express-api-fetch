8e5a6c5de2ebcef368d09b4bf502369b689ed503
Wojciech Zając <wojciechzajaaac@gmail.com>
2020-03-12 17:22:44 +0100

    Revert gmail setup

--- services/mailing-service.js
+++ services/mailing-service.js
@@ -1,32 +1,32 @@
 const nodemailer = require('nodemailer')
+const {EMAIL_SENDER_NAME} = process.env
+const {EMAIL_SENDER_PASS} = process.env
 
-module.exports = {
-    sendEmail: (assessmentOrder) => {
-        let testAccount = nodemailer.createTestAccount()
-        const transporter = nodemailer.createTransport({
-            service: 'smtp.ethereal.email',
-            port: 587,
-            secure: false,
-            auth: {
-                user: testAccount.user,
-                pass: testAccount.pass
-            }
-        })
-
-        const mailOptions = {
-            from: `"SMART ASSESSMENTS 👻 👻 👻 👻 👻" <${testAccount.user}>`,
-            to: assessmentOrder.candidate.email,
-            subject: `Start the ${assessmentOrder.offer.name}!`,
-            html: `
-                <h1>Hi ${assessmentOrder.candidate.firstName}!</h1>
-                <h2>Click <a href="https://smart-assessments.herokuapp.com/quick-test/${assessmentOrder.id}" target="_blank">here</a> to start the assessment.</h2>`
+module.exports.sendEmail = (assessmentOrder) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: EMAIL_SENDER_NAME,
+            pass: EMAIL_SENDER_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
         }
+    })
 
-        transporter.sendMail(mailOptions, function (err, info) {
-            if (err)
-                console.log(err)
-            else
-                console.log(info);
-        })
+    const mailOptions = {
+        from: '"SMART ASSESSMENTS 👻 👻 👻 👻 👻" <janosik.sr@gmail.com>',
+        to: assessmentOrder.candidate.email,
+        subject: `Start the ${assessmentOrder.offer.name}!`,
+        html: `
+            <h1>Hi ${assessmentOrder.candidate.firstName}!</h1>
+            <h2>Click <a href="https://smart-assessments.herokuapp.com/quick-test/${assessmentOrder.id}" target="_blank">here</a> to start the assessment.</h2>\``
     }
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err)
+            console.log(err)
+        else
+            console.log(info);
+    })
 }
